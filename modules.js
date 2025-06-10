08cd94536a4e081eda54c8edd529c8235ceb80c0
Charles Boswell <charliewboswell@gmail.com>
2025-06-09 21:04:41 -0400

    first push

---
+++ modules.js
@@ -0,0 +1,13 @@
+// const xyz = require('./people');
+
+// console.log(xyz.people, xyz.ages);
+// // console.log(people);
+
+
+const { people, ages } = require('./people');
+
+console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(),os.homedir());
